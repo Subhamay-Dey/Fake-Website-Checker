1cdbcd192c3c1d47b27095599776cb41612881b1
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-13 18:00:51 +0530

    Set up Express middleware for JSON and URL-encoded data handling, and integrate API routing ✅

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import express, {Application, Request, Response} from "express";
 import "dotenv/config";
+import router from "./routes/index.js";
 
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req:any, res:any) => {
     return res.send("Hey It's working.... 🙌")
-})
+});
+
+app.use("/api", router)
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
