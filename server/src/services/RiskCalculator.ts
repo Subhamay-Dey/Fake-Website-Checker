9a0377e45dac9cf912351840539000b1b5f5813b
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-13 16:38:18 +0530

    CalculateRiskScore Service done ✅

---
+++ server/src/services/RiskCalculator.ts
@@ -0,0 +1,15 @@
+
+class CalculateRiskScore {
+    static async calculateRiskScore(factors: any){
+        let score = 0;
+        if (factors.domainAge < 30) score += 30;
+        if (!factors.hasSSL) score += 20;
+        if (factors.isBlacklisted) score += 40;
+        if (factors.contentSimilarityScore > 80) score += 30;
+        if (factors.hasHiddenRedirects) score += 10;
+        if (factors.hasMaliciousScripts) score += 40;
+        return score;
+    };
+}
+
+export default CalculateRiskScore;
